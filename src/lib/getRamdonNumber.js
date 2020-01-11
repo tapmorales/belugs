8e2998017731899a06aa42ac2aaa4a49507264a6
Daniel Tapias Morales <tapmorales@gmail.com>
2020-01-11 13:40:55 +0000

    add default parameter to getRamdonNumber - return int or float

--- src/lib/getRamdonNumber.js
+++ src/lib/getRamdonNumber.js
@@ -1,4 +1,4 @@
-export default function getRamdonNumber(min, max) {
+export default function getRamdonNumber(min, max, integer = true) {
     let r = Math.random() * (max - min + 1) + min;
-    return parseInt(r);
+    return integer ? parseInt(r) : parseFloat(r);
 }
