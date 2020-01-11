177a64993e2b133e74b3d7603060b002a0407239
Daniel Tapias Morales <tapmorales@gmail.com>
2020-01-11 14:09:08 +0000

    fix getMedia

--- src/lib/getMedia.js
+++ src/lib/getMedia.js
@@ -1,7 +1,7 @@
 export default function getMedia() {
     let args = Array.from(arguments).filter(n => typeof n === "number");
 
-    if (!args.length) return "0.00";
+    if (!args.length) return 0;
     let media =
         args.reduce((current, sum) => {
             return current + sum;
